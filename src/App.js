283716748a3606372958d9695c31f81e14591ded
BigBirdsThigh <zb2007@hw.ac.uk>
2024-01-25 16:13:18 +0000

    Navbar renamed to Nav + design changes to newly named Nav

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import './App.css';
-import Navbar from './Elements/Navbar';
-import Home from './Pages/Home'; // Import the Home component
-import About from './Pages/About';
-import Designs from './Pages/Designs';
 import Footer from './Elements/Footer';
 import AnimatedRoutes from './Elements/AnimatedRoutes';
 
---
+++ src/Elements/Nav.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../StyleSheets/Navbar.css';
+import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
+function Nav() {
+    
+    return (    
+        <>
+    <Navbar sticky="top" expand="lg" className="blue">
+      <Container>
+        <h1 className="logo">
+          <span>Html Hooligans</span>
+        </h1>                        
+          <Link to="/"><Navbar.Brand><button>Home</button></Navbar.Brand></Link>
+          <Link to="/about"><Navbar.Brand><button>About</button></Navbar.Brand></Link>
+          <Link to="/designs"><Navbar.Brand><button>Designs</button></Navbar.Brand></Link>
+          <Link to="/test"><Navbar.Brand><button>Landing</button></Navbar.Brand></Link>
+        
+      </Container>
+    </Navbar>
+      </>    
+    );
+}
+
+export default Nav;
