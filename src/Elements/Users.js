61b022f0398bb47de7775cb5c038ddbc24b599a8
sibilashihab <110889822+sibilashihab@users.noreply.github.com>
2024-01-25 13:10:33 +0000

    Changed text colours in some js files for readability

--- src/Elements/Users.js
+++ src/Elements/Users.js
@@ -9,7 +9,7 @@ function Users(props) {
                     <div class="card mx-auto p-2 my-3" style={{maxWidth:"30rem"}}>
                          <h5 class="card-header">For {props.title}</h5>
                          <div class="card-body">
-                              <p class="card-text">{props.text}</p> 
+                              <p class="card-text text-dark">{props.text}</p> 
                               <Link to ="/about" className= "btn btn-secondary">→</Link>                             
                          </div>
                     </div>
